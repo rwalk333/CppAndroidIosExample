0ba6cf3a7abbc73c73527db1bc7385893550ee11
rwalker <rwalker@MacGreece.fluential.net>
2015-01-28 13:43:36 -0800

    Include Lua library in the C++ project

--- CPP/Core.cpp
+++ CPP/Core.cpp
@@ -7,10 +7,15 @@
 //
 
 #include "Core.h"
+#include "lua.h"
+#include "lauxlib.h"
+#include "lualib.h"
+
 
 const char* CPP_BASE_STRING = "cpp says hello world to %s";
 
 const char* concateneMyStringWithCppString(const char* myString) {
+
     char* concatenedString = new char[strlen(CPP_BASE_STRING) + strlen(myString)];
 	sprintf(concatenedString, CPP_BASE_STRING, myString);
 	return concatenedString;
--- CPP/Core.h
+++ CPP/Core.h
@@ -11,6 +11,7 @@
 
 #include <iostream>
 
+
 const char* concateneMyStringWithCppString(const char* myString);
 
 #endif /* defined(__HelloCpp__Core__) */
