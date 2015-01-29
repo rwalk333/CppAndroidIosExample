b181c0a6b0485c945cfdbe2a91ad91ea4d8bbe5e
rwalker <rwalker@MacGreece.fluential.net>
2015-01-28 17:20:32 -0800

    Have Lua say something to C++ and have C++ say something Objective-C

--- CPP/Core.cpp
+++ CPP/Core.cpp
@@ -7,16 +7,51 @@
 //
 
 #include "Core.h"
-#include "lua.h"
-#include "lauxlib.h"
-#include "lualib.h"
 
+extern "C"{
+ #include "lua.h"
+ #include "lauxlib.h"
+ #include "lualib.h"
+}
 
-const char* CPP_BASE_STRING = "cpp says hello world to %s";
+#include "iostream"
+
+std::string CPP_BASE_STRING = "cpp says hello world to ";
 
 const char* concateneMyStringWithCppString(const char* myString) {
-
-    char* concatenedString = new char[strlen(CPP_BASE_STRING) + strlen(myString)];
-	sprintf(concatenedString, CPP_BASE_STRING, myString);
-	return concatenedString;
+    std::string msg;
+    
+    // open the lua stack and push the echo function
+    lua_State * L = luaL_newstate();
+    luaL_openlibs(L);
+    std::string astr = "echo = function(x) return x end";
+    luaL_loadstring(L, astr.c_str());
+    int err0 = lua_pcallk(L, 0, LUA_MULTRET, 0, 0, NULL);
+    if(err0 != LUA_OK)
+    {
+        msg = lua_tolstring(L, -1, NULL);
+        return msg.c_str();
+    }
+    
+    // now, call the echo function on the input
+    lua_settop(L, 0);
+    lua_getglobal(L, "echo");
+    std::string luamessage;
+    luamessage = "lua says hello to cpp";
+    lua_pushstring(L, luamessage.c_str());
+    int err = lua_pcallk(L, 1, LUA_MULTRET, 0, 0, NULL);
+    if(err != LUA_OK)
+    {
+        msg = lua_tolstring(L, -1, NULL);
+        return msg.c_str();
+    }
+    
+    int n = lua_gettop(L);
+    if(n==1)
+    {
+        msg = lua_tolstring(L, -1, NULL);
+    }
+    
+    msg = CPP_BASE_STRING + myString + " : "+msg;
+    return msg.c_str();
 }
